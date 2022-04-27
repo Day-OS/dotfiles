9dfa5b09c4cf032243b1e3e4b74b8a664ca6891b
DayOS <43686351+Day-OS@users.noreply.github.com>
2022-04-27 08:12:31 -0300

    changewp script

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -101,7 +101,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 --wal -i dotfiles/awesome/themes/default/wallpaper/kkkk.png --backend haishoku
-theme.wallpaper = themes_path.."default/wallpaper/bruni.jpg"
+--theme.wallpaper = themes_path.."default/wallpaper/nothing.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."defaultsudo /layouts/fairhw.png"
